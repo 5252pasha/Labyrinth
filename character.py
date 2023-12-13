df9b70ba3fc25861f5b5a2e86ca1b586f8e1903c
lpopivnyak <141335054+YuriyKulyck@users.noreply.github.com>
2023-12-13 17:19:59 +0200

    GONDURAS

--- character.py
+++ character.py
@@ -5,25 +5,38 @@ class Character:
     def __init__(self, x, y, w, h, speed, texture):
         self.speed = speed
         self.texture = pygame.image.load(texture)
+        self.texture = pygame.transform.scale(self.texture, (w, h))
         self.hit_box = self.texture.get_rect()
         self.hit_box.x = x
         self.hit_box.y = y
-        self.texture = pygame.transform.scale(self.texture, (w, h))
+        self.sound_step = pygame.mixer.Sound("screamer-jumpscare-66896.mp3")
+        self.sound_step.play(-1)
+        self.sound_step.set_volume(0)
 
     def render(self, window):
         window.blit(self.texture, (self.hit_box.x, self.hit_box.y))
 
     def movement(self):
         keys = pygame.key.get_pressed()
+        is_step = False
         if keys[pygame.K_d]:
-            if self.hit_box.x < 750:
+            if self.hit_box.x < 775:
                 self.hit_box.x += self.speed
+                is_step = True
         if keys[pygame.K_a]:
             if self.hit_box.x > 0:
                 self.hit_box.x -= self.speed
+                is_step = True
         if keys[pygame.K_w]:
             if self.hit_box.y > 0:
                 self.hit_box.y -= self.speed
+                is_step = True
         if keys[pygame.K_s]:
-            if self.hit_box.y < 450:
+            if self.hit_box.y < 475:
                 self.hit_box.y += self.speed
+                is_step = True
+
+        if is_step == True:
+            self.sound_step.set_volume(1.5)
+        else:
+            self.sound_step.set_volume(0)
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import random
+
 import pygame
+import time
+
 import character
 import enemy
 from gold import Gold
@@ -10,13 +14,14 @@ pygame.mixer.init()
 pygame.mixer.music.load("jungles.ogg")
 pygame.mixer.music.play(-1)
 
+money_sound = pygame.mixer.Sound("money.ogg")
 window = pygame.display.set_mode((800, 500))
 fps = pygame.time.Clock()
 
 layout = pygame.image.load("background.jpg")
 layout = pygame.transform.scale(layout, (800, 500))
 
-drachilaN1 = character.Character(400, 150, 32, 35, 2, "hero.png")
+drachilaN1 = character.Character(400, 150, 32, 34, 2, "hero.png")
 dubinaN1 = enemy.Enemy(110, 220, 45, 47, 4, "cyborg.png", 100, 200, 300, 300)
 gold = Gold(400, 450, 50, 50, "treasure.png")
 game = True
@@ -25,18 +30,19 @@ walls = []
 walls.append(Wall(43, 47, 711, 21, (255, 255, 1)))
 walls.append(Wall(455, 238, 31, 201, (255, 90, 10)))
 walls.append(Wall(1, 488, 373, 14, (0, 25, 255)))
-walls.append(Wall(42, 46, 22, 404, (142, 242, 1)))
-walls.append(Wall(99, 101, 11, 388, (0, 72, 144)))
+walls.append(Wall(32, 46, 22, 404, (142, 242, 1)))
+walls.append(Wall(99, 109, 11, 380, (0, 72, 144)))
 walls.append(Wall(385, 238, 70, 200, (0, 255, 255)))
 walls.append(Wall(455, 105, 31, 93, (250, 90, 10)))
-walls.append(Wall(330, 67, 11, 383, (144, 25, 144)))
+walls.append(Wall(330, 70, 11, 380, (144, 25, 144)))
 walls.append(Wall(371, 238, 16, 262, (72, 200, 111)))
 walls.append(Wall(371, 67, 16, 131, (72, 200, 144)))
 walls.append(Wall(386, 188, 70, 10, (0, 72, 144)))
-walls.append(Wall(91, 101, 11, 388, (0, 72, 144)))
-walls.append(Wall(91, 101, 11, 388, (0, 72, 144)))
-while game:
+walls.append(Wall(91, 109, 11, 380, (0, 72, 144)))
+walls.append(Wall(91, 109, 11, 380, (0, 72, 144)))
 
+start_time = time.time()
+while game:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
@@ -53,6 +59,16 @@ while game:
     drachilaN1.render(window)
     dubinaN1.render(window)
     gold.render(window)
+    for wall in walls:
+        if drachilaN1.hit_box.colliderect(wall.rect):
+            game = False
+    if time.time() - start_time > 0.015:
+        for wall in walls:
+            wall.color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+        start_time = time.time()
+    if gold.hit_box.colliderect(drachilaN1.hit_box):
+        money_sound.play()
+        gold.hit_box.x += 100000000
     for wall in walls:
         wall.render(window)
     pygame.display.flip()
